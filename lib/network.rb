7462e93a037ebaf9e061fc7f7a1ef48a2afa1c14
Caryn Keffer <carynkeffer@gmail.com>
2020-12-15 12:16:31 -0500

    add network class and test

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,34 @@
- 
+class Network
+  attr_reader :name,
+              :shows
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    mains = []
+    shows.each do |show|
+      show.characters.each do |character|
+       mains << character if character.salary > 500000
+      end
+    end
+    mains
+  end
+
+  def actors_by_show
+    by_show = Hash.new
+    shows.each do |show|
+      actor_names = []
+      show.characters.each do |character|
+        actor_names << character.actor
+      end
+      by_show[show] = actor_names
+    end
+    by_show
+  end
+end
--- test/network_test.rb
+++ test/network_test.rb
@@ -1,1 +1,34 @@
- 
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    nbc = Network.new("NBC")
+
+    assert_instance_of Network, nbc
+    assert_equal "NBC", nbc.name
+    assert_equal [], nbc.shows
+  end
+
+  def test_it_has_shows
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
+    assert_equal [michael_knight, kitt, leslie_knope, ron_swanson], nbc.main_characters
+    # require "pry"; binding.pry
+    expected = {knight_rider => ["David Hasselhoff", "William Daniels"], parks_and_rec => ["Amy Poehler", "Nick Offerman"]}
+    assert_equal expected, nbc.actors_by_show
+  end
+end
